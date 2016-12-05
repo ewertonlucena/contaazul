69c17ac52ba2adb1ba5f25e77341a4891fb3b727
uploja <uploja@DESKTOP-D3H1UNA>
2016-12-05 17:04:11 -0300

    Ajustes no layout do login e para celulares

--- views/login.php
+++ views/login.php
@@ -7,7 +7,9 @@
     <body>
         <div class="loginarea">
             <form method="POST">
+                E-mail
                 <input Type="email" name="email" placeholder="Digite ser e-mail"/>
+                Senha
                 <input Type="password" name="password" placeholder="Digite sua senha"/>
                 <input Type="submit" value="Entrar" />
                 <br/>
